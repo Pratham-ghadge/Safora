1dbf01e2272e8de0018cb6ce93d4f93c2d695d64
Pratham-ghadge <prathamtech24@gmail.com>
2025-04-03 21:43:24 +0530

    updated project

--- myproject/myapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from decouple import config
-
-# Create your views here.
-def index(request):
-    context = {
-        'google_maps_api_key': config('GOOGLE_MAPS_API_KEY')  # Corrected the syntax
-    }
-    return render(request, 'index.html', context)
